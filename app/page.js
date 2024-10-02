9a666b723e3be26a14dcb26574be8354080cce06
03-Abhi <9818abhi@gmail.com>
2024-10-02 22:55:13 +0530

    deploy commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import ObjectDetection from "@/components/object-detection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-8 ">
+      <h1 className="gradient-title font-semibold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
+        Object Detection System
+        </h1>
+        <ObjectDetection />
+     
+    </main>
+  );
+}
